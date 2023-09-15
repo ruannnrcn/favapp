3c26918d422d7683a6ad2206c3ef0d36a71e86a7
Pedro Ruan <pedro.ruan251202@gmail.com>
2023-09-15 09:39:24 -0300

    att botao de confirmação

--- src/componentes/Conteudo/index.js
+++ src/componentes/Conteudo/index.js
@@ -21,14 +21,17 @@ const Estados = () => {
             <h1>Estados</h1>
             <h1>{nome}</h1>
             <h1>{url}</h1>
-            <input type="text" value={nome} onChange={(e)=>handleInput(e.target.value)} />
-            <br />
-            <input type="text" value={url} onChange={(e)=>setUrl(e.target.value)} />
-            <button onClick={()=>adicionarFavorito(nome,url)}>Adicionar</button>
-            <h1>Favoritos</h1>
-            <ul>
-            { favoritos.map( (fav) => <li key={fav.url}>{ fav.nome } <br />{ fav.url }</li>) }
-            </ul>
+
+            <form onSubmit={()=>alert("Site favorito adicionado!")}>
+              <input type="text" value={nome} onChange={(e)=>handleInput(e.target.value)} />
+              <br />
+              <input type="text" value={url} onChange={(e)=>setUrl(e.target.value)} />
+              <button onClick={()=>adicionarFavorito(nome,url)}>Adicionar</button>
+              <h1>Favoritos</h1>
+              <ul>
+              { favoritos.map( (fav) => <li key={fav.url}>{ fav.nome } <br />{ fav.url }</li>) }
+              </ul>
+            </form>
         </div>
     );
 };
