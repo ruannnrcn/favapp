fc93a5a2adc4283b05b5bfee607144b077a53f30
Pedro Ruan <pedro.ruan251202@gmail.com>
2023-09-15 16:22:22 -0300

    mudanças no css

--- src/componentes/Conteudo/index.js
+++ src/componentes/Conteudo/index.js
@@ -3,36 +3,55 @@ import { useState } from "react";
 import styles from "./Conteudo.module.css";
 
 const Estados = () => {
-    const [nome, setNome]=useState("Google")
-    const [url, setUrl]=useState("https://www.google.com")
-    const [favoritos, setFavoritos]=useState([])
+  const [nome, setNome] = useState("Google");
+  const [url, setUrl] = useState("https://www.google.com");
+  const [favoritos, setFavoritos] = useState([]);
 
-    function adicionarFavorito(nome, url){
-        let favorito={nome,url}
-        setFavoritos([...favoritos,favorito])
-        console.log(favoritos)
-    }
+  function adicionarFavorito(nome, url) {
+    let favorito = { nome, url };
+    setFavoritos([...favoritos, favorito]);
+    console.log(favoritos);
+  }
 
-    function handleInput(valor){
-        setNome(valor)
+  function handleInput(valor) {
+    setNome(valor);
+  }
+  return (
+    <div className={styles.main}>
+      <h2 className={styles.favoritosTitle}>Lista de Favoritos:</h2>
 
-    }
-    return(
-        <div>
-            <h1 className={styles.favoritosTitle}>Lista de Favoritos:</h1>
-            <h1>{nome}</h1>
-            <h1>{url}</h1>
-
-            <form onSubmit={()=>alert("Site favorito adicionado!")}>
-              <input type="text" value={nome} onChange={(e)=>handleInput(e.target.value)} />
-              <br />
-              <input type="text" value={url} onChange={(e)=>setUrl(e.target.value)} />
-              <button onClick={()=>adicionarFavorito(nome,url)}>Adicionar</button>
-              <ul>
-              { favoritos.map( (fav) => <li key={fav.url}>{ fav.nome } <br />{ fav.url }</li>) }
-              </ul>
-            </form>
+      <div className={styles.containerDados}>
+        <div className={styles.containerDadosIndividual}>
+          <h1 className={styles.fontTitle}>Nome do Site:</h1>
+          <h2>{nome}</h2>
         </div>
-    );
+        <div className={styles.containerDadosIndividual}>
+          <h1 className={styles.fontTitle}>Url do Site:</h1>
+          <h2>{url}</h2>
+        </div>
+      </div>
+
+      {/* <form onSubmit={()=>alert("Site favorito adicionado!")}> */}
+      <input
+        type="text"
+        value={nome}
+        onChange={(e) => handleInput(e.target.value)}
+      />
+      <br />
+      <input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
+      <button onClick={() => adicionarFavorito(nome, url)}>
+        Adicionar Favorito
+      </button>
+      <ul>
+        {favoritos.map((fav) => (
+          <li key={fav.url}>
+            {fav.nome} <br />
+            {fav.url}
+          </li>
+        ))}
+      </ul>
+      {/* </form> */}
+    </div>
+  );
 };
 export default Estados;
